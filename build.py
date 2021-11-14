62f090ab8e480b4b54d75e03e7efb0434682f719
cedric <dabrao@gmail.com>
2021-11-14 10:04:26 +0100

    adding the GitHub building script.

--- build.py
+++ build.py
@@ -24,6 +24,10 @@ class host_t:
 		#---Zero---#
 		self.install_pfx	= user_args.install_pfx.absolute()
 
+	def autoreconf(self):
+		"""Update generated configuration files."""
+		run("autoreconf", "-f", "-i")
+
 	def configure(self, triplet, *args):
 		"""Configure the build."""
 		install_pfx	= self.install_pfx.joinpath(triplet)
@@ -81,11 +85,11 @@ class host_clg_pandeb9_t(host_t):
 		]
 		for configure_idx in range(0, len(configure_arg_arr)):
 			configure_arg	= configure_arg_arr[configure_idx]
+			self.autoreconf()
 			self.configure(*configure_arg)
 			self.make()
 			self.install()
 			self.clean()
-
 
 class host_clg_macos_t(host_t):
 	def __del__(self):
@@ -113,6 +117,26 @@ class host_clg_macos_t(host_t):
 			self.install()
 			self.clean()
 
+class host_github_linux_t(host_clg_pandeb9_t):
+	def __del__(self):
+		"""The destructor."""
+		pass
+
+	def __init__(self, user_args):
+		"""The main constructor."""
+		#---Call parent constructor---#
+		super().__init__(user_args)
+
+class host_github_macos_t(host_clg_macos_t):
+	def __del__(self):
+		"""The destructor."""
+		pass
+
+	def __init__(self, user_args):
+		"""The main constructor."""
+		#---Call parent constructor---#
+		super().__init__(user_args)
+
 
 
 
@@ -121,6 +145,8 @@ class host_clg_macos_t(host_t):
 host_names	= {
 	"clg-pandeb9"	: host_clg_pandeb9_t,
 	"clg-macos"		: host_clg_macos_t,
+	"github-linux"	: host_github_linux_t,
+	"github-macos"	: host_github_macos_t,
 }
 
 #---Parse the command line---#
